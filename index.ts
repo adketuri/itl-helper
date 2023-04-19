de1dc5cc389c48e1670ea5963d7e12f2f066e056
Andrew Keturi <andrewketuri@gmail.com>
2023-04-19 15:22:34 -0700

    Fetch leaderboard

--- index.ts
+++ index.ts
@@ -9,6 +9,15 @@ async function fetchEntrant(id: number) {
   return data;
 }
 
+async function fetchLeaderboard() {
+  console.log("Fetching....");
+  const response = await fetch(
+    `https://itl2023.groovestats.com/api/entrant/leaderboard`
+  );
+  const data = await response.json();
+  return data;
+}
+
 fetchEntrant(302).then((entrant: EntrantResponse) =>
   console.log(JSON.stringify(entrant))
 );
