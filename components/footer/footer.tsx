3c42f995b397cd79fb4874589b26068b138c6e85
Andrew Keturi <adketuri@gmail.com>
2023-04-21 16:01:31 -0700

    Add footer icons

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,4 +1,7 @@
-import { useColorModeValue, Flex, Container, Text, Link } from "@chakra-ui/react";
+import { useColorModeValue, Flex, Container, Text, Link, IconButton } from "@chakra-ui/react";
+
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai"
+
 import NextLink from "next/link";
 
 export const Footer = () => {
@@ -7,11 +10,21 @@ export const Footer = () => {
     <>
       <Flex bg={bg} py={8} direction={{ base: "column", md: "row" }}>
         <Container maxW="container.md">
-          <Flex direction="column">
-            <Text variant="sub">by zexyu</Text>
-            <Text variant="sub"><Link variant="outgoing" as={NextLink} href={"https://ko-fi.com/zexyu"}>
-              Donate
-            </Link></Text>
+          <Flex>
+            <Flex direction="column">
+              <Text variant="sub">by zexyu</Text>
+              <Text variant="sub"><Link isExternal variant="outgoing" as={NextLink} href={"https://ko-fi.com/zexyu"}>
+                Donate
+              </Link></Text>
+            </Flex>
+            <Flex ml="auto">
+              <Link isExternal href='https://twitter.com/zexyu'>
+                <IconButton color="gray.500" variant="link" aria-label="Twitter" size="sm" as={AiOutlineTwitter} />
+              </Link>
+              <Link isExternal href='https://github.com/adketuri/itl-helper' ml={3}>
+                <IconButton color="gray.500" variant="link" aria-label="GitHub" size="sm" as={AiFillGithub} />
+              </Link>
+            </Flex>
           </Flex>
         </Container>
       </Flex>
