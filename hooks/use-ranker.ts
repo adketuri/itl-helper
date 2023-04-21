9067b48e15eba695e42ae116f3d9a5a7b6057277
Andrew Keturi <adketuri@gmail.com>
2023-04-20 21:16:26 -0700

    Update header

--- hooks/use-ranker.ts
+++ hooks/use-ranker.ts
@@ -3,6 +3,14 @@ import { Chart, ChartScore, Entrant, EntrantResponse, EntrantScore, Leaderboard,
 
 const chartMap = new Map<string, Chart>();
 const apiUrl = "https://itl2023.groovestats.com/api/entrant";
+const options = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+}
+
 
 async function fetchEntrant(id: number) {
   console.log(`Fetching Entrant ${id}....`);
@@ -14,13 +22,13 @@ async function fetchEntrant(id: number) {
 
 async function fetchLeaderboard() {
   console.log("Fetching Leaderboard....");
-  const response = await fetch(`${apiUrl}/leaderboard`);
+  const response = await fetch(`${apiUrl}/leaderboard`, options);
   const data = await response.json();
   return data;
 }
 
 async function fetchPeerScores(entrants: EntrantScore[]) {
-  console.log("Fetching peers...");
+  console.log("Fetching peers...", options);
   const peers: EntrantResponse[] = await Promise.all(entrants.map((entrantScore) => fetchEntrant(entrantScore.entrant.id)));
   return peers.map(peer => peer.data.topScores.sort((s1, s2) => s2.points - s1.points));
 }
